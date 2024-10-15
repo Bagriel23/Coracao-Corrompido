ae2f47a9ee484523dbfd8330f23bc52b2b8dd740
Gabriel Dantas <gabrielsoudantas@gmail.com>
2024-10-15 16:01:50 -0400

    Tinha dado erro

--- Assets/Assets/Coringa.cs
+++ Assets/Assets/Coringa.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class coringa : MonoBehaviour
+public class Coringa : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
