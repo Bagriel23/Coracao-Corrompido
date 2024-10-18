9eb54521559b0da1d9f647773fd59fd1a6d31525
Gabriel Dantas <gabrielsoudantas@gmail.com>
2024-10-18 14:08:47 -0400

    Sistema de HP(Health Bar)

--- Assets/Scripts/player/PlayerLife.cs
+++ Assets/Scripts/player/PlayerLife.cs
@@ -1,34 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
-    public int life;
+    public Slider hpSlider, easeHpSlider;
+    [SerializeField] private float maxHP;
+    [SerializeField] private float currentHP;
+    private float smoothVelocity = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHP = maxHP;
+        hpSlider.value = currentHP;
+        easeHpSlider.value = currentHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (life <= 0)
+        UpdateHP();
+        testDamage();        
+        easeEffect();
+
+        currentHP =  Mathf.Clamp(currentHP, 0, maxHP);
+    }
+
+    private void UpdateHP()
+    {
+        if (hpSlider.value != currentHP)
         {
-            EndGame();
+            hpSlider.value = currentHP;
         }
     }
 
-    public void TakeDamage(int damage)
+    private void easeEffect()
     {
-        this.life -= damage;
-        Debug.Log("Player Sofreu " + damage + " de dano");
+        if (hpSlider.value != easeHpSlider.value)
+        {
+            easeHpSlider.value = Mathf.SmoothDamp(easeHpSlider.value, currentHP, ref smoothVelocity, 0.09f);
+        }   
     }
 
-    public void EndGame()
+    public void testDamage()
     {
-        Application.Quit();
-
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        Debug.Log("Player Sofreu " + damage + " de dano. Vida atual: " + currentHP + ".");
     }
 }
